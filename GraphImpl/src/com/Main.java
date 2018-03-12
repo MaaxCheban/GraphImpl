a82cdb0133697f1e0244128f8b9c6b348427184e
Maax <maaxcheban@gmail.com>
2018-03-12 22:43:07 +0200

    find method

--- GraphImpl/src/com/Main.java
+++ GraphImpl/src/com/Main.java
@@ -1,17 +1,35 @@
 package com;
 
-        import com.Graph.*;
+import com.Graph.*;
 
-        import java.util.LinkedList;
-        import java.util.List;
-        import com.approach1.Graph;
-        import com.approach1.Vertex;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
-        public static void main(String[] args) {
-                // write your code here
+    public static void main(String[] args) {
+	// write your code here
 
+        Graph<String> graph = new Graph<>();
 
+        graph.addVertex(new Vertex<>("Bangalore"));
+        graph.addVertex(new Vertex<>("Hong Kong"));
+        graph.addVertex(new Vertex<>("LA"));
+        graph.addVertex(new Vertex<>("SFO"));
+
+        graph.addEdge(new Vertex<>("Bangalore"), new Vertex<>("Hong Kong"), 50);
+        graph.addEdge(new Vertex<>("Bangalore"), new Vertex<>("LA"), 70);
+        graph.addEdge(new Vertex<>("Bangalore"), new Vertex<>("SFO"), 100);
+        graph.addEdge(new Vertex<>("Hong Kong"), new Vertex<>("LA"), 60);
+        graph.addEdge(new Vertex<>("LA"), new Vertex<>("SFO"), 20);
+
+        DijkstraAlgorithm<String> algorithm = new DijkstraAlgorithm<>(graph);
+        List<Vertex<String>> vertexes;
+        vertexes = graph.findShortestWay(algorithm);
+
+        for(Vertex vertex : vertexes ){
+            System.out.println(vertex);
         }
+
+    }
 }
