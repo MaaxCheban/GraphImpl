85b81b2f2946f9dce7556c6f3d69bd5e29ffc69b
Rostyk Boiko <rosstyk@gmail.com>
2018-03-12 20:18:02 +0200

    Test: Simple Test

--- GraphImpl/src/com/test/TestMain.java
+++ GraphImpl/src/com/test/TestMain.java
@@ -1,12 +1,11 @@
 package com.test;
 
-
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 
 public class TestMain {
     @Test
     public void testExecute() {
-
+        assertEquals(2+2,4);
     }
 }
